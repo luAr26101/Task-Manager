d83a850957f4cc85c39251e548bbdedf8e1c6a6b
luAr26101 <raul.cosmin.26@icloud.com>
2025-07-23 11:21:37 +0300

    Add props to task card component.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import "./App.css";
 import TaskCard from "./components/task-card/TaskCard";
 
 function App() {
+  const data = [
+    {
+      id: "T-1",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2025, 5, 22),
+    },
+
+    {
+      id: "T-2",
+      status: "In Progress",
+      name: "Create a Design System for web app",
+      dueDate: new Date(2025, 5, 1),
+    },
+
+    {
+      id: "T-3",
+      status: "Completed",
+      name: "Create a Design System for web app",
+      dueDate: new Date(2025, 6, 10),
+    },
+  ];
+
   return (
-    <div className="app-container">
-      <TaskCard />
+    <div className='app-container'>
+      <TaskCard {...data[0]} />
     </div>
   );
 }
--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,24 +1,19 @@
 import "./TaskCard.css";
 
-function TaskCard() {
-  const taskId = "T-2";
-  const taskStatus = "Todo";
-  const taskName = "Create a Design System for Enum Workspace.";
-  const dueDate = new Date(2022, 5, 23);
-
+function TaskCard({ id, status, name, dueDate }) {
   return (
-    <div className="card-wrapper">
-      <div className="card-header">
-        <p className="task-id">{taskId}</p>
-        <div className="badge">
-          <p>{taskStatus}</p>
+    <div className='card-wrapper'>
+      <div className='card-header'>
+        <p className='task-id'>{id}</p>
+        <div className='badge'>
+          <p>{status}</p>
         </div>
       </div>
-      <div className="card-content">
-        <p>{taskName}</p>
+      <div className='card-content'>
+        <p>{name}</p>
       </div>
-      <div className="card-footer">
-        <div className="due-date">
+      <div className='card-footer'>
+        <div className='due-date'>
           <p>Due Date</p>
           <p>{dueDate.toLocaleDateString()}</p>
         </div>

d8c348f48a6306c5fcb7b68cb98c1faaaa5cffdc
luAr26101 <raul.cosmin.26@icloud.com>
2025-07-23 12:41:32 +0300

    Add simple event handler.

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -1,18 +1,19 @@
-import "./TaskCard.css";
 import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
+import "./TaskCard.css";
 
 function TaskCard(props) {
   return (
-    <div className="card-wrapper">
-      <div className="card-header">
-        <p className="task-id">{props.id}</p>
+    <div className='card-wrapper'>
+      <div className='card-header'>
+        <p className='task-id'>{props.id}</p>
         <Badge status={props.status} />
       </div>
-      <div className="card-content">
+      <div className='card-content'>
         <p>{props.name}</p>
       </div>
-      <div className="card-footer">
+      <div className='card-footer'>
+        <button onClick={() => console.log("Button clicked")}>Click</button>
         <DateContainer date={props.dueDate} />
       </div>
     </div>
